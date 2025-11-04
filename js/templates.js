790d7f3188e7b4168acc3701cffc69947d078146
akin <akin@femka.de>
2025-11-04 16:34:12 +0100

    changes because of an error  in templates.js

--- js/templates.js
+++ js/templates.js
@@ -5,7 +5,7 @@ function getPokemonCardTemplate(name, image, types) {
         typesHTML += `<span class="type-badge" style="background: var(--type-${typeName})">${typeName}</span>`;
     }    
     return `
-        <div class="pokemon-card" style="background: var(--type-${types[0].type.name})>
+        <div class="pokemon-card" style="background: var(--type-${types[0].type.name})">
                     <h3 class="pokemon-name">${name}</h3>
                     <img src="${image}" alt="${name}">
                     <div class="pokemon-types">${typesHTML}</div>
