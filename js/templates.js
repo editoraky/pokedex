0cdc83b01cfd4da279142783e092e0e7e4047167
akin <akin@femka.de>
2025-11-04 16:27:31 +0100

    html template for showing pokemons by name, image, and types

---
+++ js/templates.js
@@ -0,0 +1,14 @@
+function getPokemonCardTemplate(name, image, types) {
+    let typesHTML = "";
+    for (let i = 0; i < types.length; i++) {
+        const typeName = types[i].type.name;
+        typesHTML += `<span class="type-badge" style="background: var(--type-${typeName})">${typeName}</span>`;
+    }    
+    return `
+        <div class="pokemon-card" style="background: var(--type-${types[0].type.name})>
+                    <h3 class="pokemon-name">${name}</h3>
+                    <img src="${image}" alt="${name}">
+                    <div class="pokemon-types">${typesHTML}</div>
+                </div>
+    `;
+}
